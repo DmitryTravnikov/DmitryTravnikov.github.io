417a76142ef7985cf52290793cb6ba1a3de2b9dc
Dmitry Travnikov <travnikov@outlook.com>
2019-01-06 13:55:04 +0300

    commit

--- js/common.js
+++ js/common.js
@@ -136,15 +136,6 @@
 
 // });//doc ready end
 
-document.body.onload = function() {
-	setTimeout(function() {
-		var preloader = document.getElementById('page-preloader');
-		if (!preloader.classList.contains('done')) {
-			preloader.classList.add('done');
-		}
-	}, 1000);
-}
-
 $(document).ready(function() {
 
 	//prevent default
