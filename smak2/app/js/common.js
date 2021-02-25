e945881d380ada3d689e2abb8e9b4e13e2fb0b61
Dmitry Travnikov <travnikov@outlook.com>
2021-02-25 15:11:35 +0300

    smak2 main is finished

--- smak2/app/js/common.js
+++ smak2/app/js/common.js
@@ -1,40 +1,4 @@
 'use strict';
-// $(document).ready(function() {
-
-//  //Таймер обратного отсчета
-//  //Документация: http://keith-wood.name/countdown.html
-//  //<div class="countdown" date-time="2015-01-07"></div>
-//  var austDay = new Date($(".countdown").attr("date-time"));
-//  $(".countdown").countdown({until: austDay, format: 'yowdHMS'});//таймер end
-
-//  //Попап менеджер FancyBox
-//  //Документация: http://fancybox.net/howto
-//  //<a class="fancybox"><img src="image.jpg" /></a>
-//  //<a class="fancybox" data-fancybox-group="group"><img src="image.jpg" /></a>
-//  $(".fancybox").fancybox();//fancybox end
-
-//  //Навигация по Landing Page
-//  //$(".top_mnu") - это верхняя панель со ссылками.
-//  //Ссылки вида <a href="#contacts">Контакты</a>
-//  $(".top_mnu").navigation();//навигация end
-
-//  //Добавляет классы дочерним блокам .block для анимации
-//  //Документация: http://imakewebthings.com/jquery-waypoints/
-//  $(".block").waypoint(function(direction) {
-// 	 if (direction === "down") {
-// 		 $(".class").addClass("active");
-// 	 } else if (direction === "up") {
-// 		 $(".class").removeClass("deactive");
-// 	 };
-//  }, {offset: 100});//end
-
-//  //Плавный скролл до блока .div по клику на .scroll
-//  //Документация: https://github.com/flesler/jquery.scrollTo
-//  $("a.scroll").click(function() {
-// 	 $.scrollTo($(".div"), 800, {
-// 		 offset: -90
-// 	 });
-//  });//плавный скролл end
 
 //  //Каруселька
 //  //Документация: http://owlgraphic.com/owlcarousel/
@@ -63,17 +27,6 @@
 //  $(".prev_button").click(function(){
 // 	 owl.trigger("owl.prev");
 //  });//карусель end
-
-//  //Кнопка "Наверх"
-//  //Документация:
-//  //http://api.jquery.com/scrolltop/
-//  //http://api.jquery.com/animate/
-//  $("#top").click(function () {
-// 	 $("body, html").animate({
-// 		 scrollTop: 0
-// 	 }, 800);
-// 	 return false;
-//  });//кнопка "наверх" end
 	
 //  //Аякс отправка форм
 //  //Документация: http://api.jquery.com/jquery.ajax/
@@ -112,30 +65,6 @@
 
 // 	});//ajax вкладки end
 
-// 	// Аккордеон
-// 	$(document).ready(function() {
-		
-// 	 $('.service2>.wrapper>.block-2>article').not(':first-of-type').hide();
-	 
-		
-// 		$('.service2>.wrapper>.block-2>.headline').click(function() {
-			
-// 			var findArticle = $(this).next();
-// 			var findWrapper = $(this).closest('.block-2');
-			
-// 			if (findArticle.is(':visible')) {
-// 				findArticle.slideUp(50);
-// 			}
-// 			else {
-// 				findWrapper.find('>article').slideUp(50);
-// 				findArticle.slideDown(50);
-// 			}
-// 		});
-
-// 	});//аккордеон end
-
-// });//doc ready end
-
 $(document).ready(function() {
 
 	//prevent default
@@ -150,6 +79,6 @@ $(document).ready(function() {
 
 });//doc.ready end
 
-let cLog = function(n) {
+var cLog = function(n) {
 	return console.log(n);
 };//console.log
